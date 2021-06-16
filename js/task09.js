7cf88cd01f2c95f67b16209b74c3d46bb3c58d60
Denis Makarets <makaretsdenis@gmail.com>
2021-06-16 16:11:25 +0300

    second commit

--- js/task09.js
+++ js/task09.js
@@ -1,1 +1,9 @@
 const inputNumber = prompt('Type the number of 3 digits');
+let digitArr = [];
+
+digitArr[0] = inputNumber % 10;
+const middleNumber = Math.trunc(inputNumber / 10);
+digitArr[1] = middleNumber % 10;
+digitArr[2] = Math.trunc (middleNumber / 10);
+
+alert(String(digitArr[0])+String(digitArr[1])+String(digitArr[2]));
